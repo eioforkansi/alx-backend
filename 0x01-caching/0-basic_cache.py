c1236bcd27ef61f632dddfb8ab9e25c67178578f
eioforkansi <eioforkansi@gmail.com>
2024-10-29 20:11:53 +0100

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that provides BaseCache class.
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """
+    Basic Caching system.
+    """
+    def __init__(self):
+        """
+        Initialize parent class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assign value to a key in the dictionary.
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return value in a the dictionary by key.
+        """
+        if key is None or self.cache_data[key] is None:
+            return None
+        return self.cache_data.get(key)
+    
